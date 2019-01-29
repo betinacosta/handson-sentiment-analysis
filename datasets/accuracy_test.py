cad7f32de6e7adf1a2da81a2791eca6839251747
Amitrajit Bose <amitrajitbose9@gmail.com>
2019-01-29 20:28:18 +0530

    Add negative accuracy testing
    Updated accuracy_test.py by adding negative text accuracy testing method.

--- datasets/accuracy_test.py
+++ datasets/accuracy_test.py
@@ -5,7 +5,6 @@ from textblob import TextBlob
 def positive_analysis():
     count = 0
     correct = 0
-
     with open("datasets/positive.txt","r") as f:
         for line in f.read().split('\n'):
             analysis = TextBlob(line)
@@ -15,4 +14,17 @@ def positive_analysis():
             
     print("Positive accuracy = {}% via {} samples".format(correct/count*100.0, count))
 
+def negative_analysis():
+    count = 0
+    correct = 0
+    with open("datasets/negative.txt","r") as f:
+        for line in f.read().split('\n'):
+            analysis = TextBlob(line)
+            if analysis.sentiment.polarity < 0:
+                correct += 1
+            count +=1
+            
+    print("Negative accuracy = {}% via {} samples".format(correct/count*100.0, count))
+    
 positive_analysis()
+negative_analysis()
